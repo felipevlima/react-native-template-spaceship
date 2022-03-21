b23aaada6ba5c3e1eded9a7cb06c616a5166b3cb
Lucas Augusto <lucasaugustscode@gmail.com>
2022-03-20 23:41:17 -0300

    chore: plugins and path alias babel.config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,30 @@
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true)
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      ['inline-dotenv'],
+      'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.ts', '.tsx', '.jsx', '.js', '.json'],
+          alias: {
+            '@routes': './src/routes',
+            '@screens': './src/screens',
+            '@components': './src/components',
+            '@libs': './src/libs',
+            '@contexts': './src/contexts',
+            '@dtos': './src/dtos',
+            '@services': './src/services',
+            '@hooks': './src/hooks',
+            '@styles': './src/styles',
+            '@assets': './src/assets',
+            '@utils': './src/utils'
+          }
+        }
+      ]
+    ]
   }
 }
